a7a0236fef102fb9235d7a85d1424fb0734ed90d
JoshJGT <joshj777@gmail.com>
2011-02-17 15:37:52 -0500

    Finished with the initial draft of the new DB scheme.

--- NewScheme.sql
+++ NewScheme.sql
@@ -3,7 +3,11 @@ CREATE SCHEMA IF NOT EXISTS `agora` DEFAULT CHARACTER SET utf8 COLLATE utf8_gene
 USE `agora` ;
 SET storage_engine=INNODB;
 
+DROP TABLE IF EXISTS `agora`.`connections`;
+DROP TABLE IF EXISTS `agora`.`arguments`;
+DROP TABLE IF EXISTS `agora`.`connectionTypes` ;
 DROP TABLE IF EXISTS `agora`.`nodes`;
+DROP TABLE IF EXISTS `agora`.`nodetypes`;
 DROP TABLE IF EXISTS `agora`.`maps`;
 DROP TABLE IF EXISTS `agora`.`users`;
 
@@ -40,14 +44,22 @@ CREATE  TABLE IF NOT EXISTS `agora`.`maps` (
   PRIMARY KEY (`map_id`) ,
   INDEX `user_id` (`user_id` ASC) ,
   FOREIGN KEY (`user_id`)
-    REFERENCES `agora`.`users` (`user_id` )
+    REFERENCES `agora`.`users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 -- -------------------------------------------------------
+-- Table `agora`.`node_types`
+-- -------------------------------------------------------
+CREATE TABLE IF NOT EXISTS `agora`.`node_types` (
+  `nodetype_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(30) NOT NULL,
+  PRIMARY KEY (`nodetype_id`));
+	
+-- -------------------------------------------------------
 -- Table `agora`.`nodes`
 -- -------------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `agora`.`nodes` (
+CREATE TABLE IF NOT EXISTS `agora`.`nodes` (
   `node_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
   `user_id` INT UNSIGNED NOT NULL ,
   `map_id` INT UNSIGNED NULL , 
@@ -61,13 +73,74 @@ CREATE  TABLE IF NOT EXISTS `agora`.`nodes` (
   PRIMARY KEY (`node_id`) ,
   INDEX `user_id` (`user_id` ASC) ,
   INDEX `map_id` (`map_id` ASC) ,
-  FOREIGN KEY (`user_id` )
-	REFERENCES `agora`.`users` (`user_id` )
+  INDEX `node_type` (`node_type` ASC) ,
+  FOREIGN KEY (`user_id`)
+	REFERENCES `agora`.`users` (`user_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  FOREIGN KEY (`map_id` )
-	REFERENCES `agora`.`maps` (`map_id` )
+  FOREIGN KEY (`map_id`)
+	REFERENCES `agora`.`maps` (`map_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  FOREIGN KEY(`node_type`)
+    REFERENCES `agora`.`node_types` (`nodetype_id`)
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION);
+
+-- -----------------------------------------------------
+-- Table `agora`.`connectionTypes`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `agora`.`connectionTypes` (
+  `type_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(60) NULL ,
+  PRIMARY KEY (`type_id`));
+-- Note that one of the connection types is "comments on"
+-- Others include "objects to", "refutes", "proposes an amendment", etc.
+-- That's why this is "connection types" and not "argument types".
+
+
+-- -----------------------------------------------------
+-- Table `agora`.`arguments`
+-- -----------------------------------------------------
+
+CREATE  TABLE IF NOT EXISTS `agora`.`arguments` (
+  `argument_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `map_id` INT UNSIGNED NULL,
+  `node_id` INT UNSIGNED NULL ,
+  `type_id` INT UNSIGNED NULL ,
+  PRIMARY KEY (`argument_id`) ,
+  INDEX `map_id` (`map_id` ASC) ,
+  INDEX `node_id` (`node_id` ASC) ,
+  INDEX `type_id` (`type_id` ASC) ,
+  FOREIGN KEY (`map_id`)
+    REFERENCES `agora`.`maps` (`map_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  FOREIGN KEY (`node_id`)
+    REFERENCES `agora`.`nodes` (`node_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  FOREIGN KEY (`type_id`)
+    REFERENCES `agora`.`connectionTypes` (`type_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
---node_type includes things like:
--- "universal reason", "particular reason", "comment", "inference", etc.
+-- The node_id is the claim.
+
+-- -----------------------------------------------------
+-- Table `agora`.`connections`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `agora`.`connections` (
+	`connection_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`argument_id` INT UNSIGNED NOT NULL,
+	`node_id` INT UNSIGNED NOT NULL,
+	PRIMARY KEY (`connection_id`),
+	INDEX `argument_id` (`argument_id` ASC),
+	INDEX `node_id` (`node_id` ASC),
+	FOREIGN KEY (`argument_id`)
+	  REFERENCES `agora`.`arguments` (`argument_id`)
+	  ON DELETE NO ACTION
+	  ON UPDATE NO ACTION,
+	FOREIGN KEY (`node_id`)
+	  REFERENCES `agora`.`nodes` (`node_id`)
+	  ON DELETE NO ACTION
+	  ON UPDATE NO ACTION);
