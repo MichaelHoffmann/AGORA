c02d3dc26abb22b932224327671bc159a2ae0030
JoshJGT <joshj777@gmail.com>
2011-02-17 13:36:22 -0500

    First bit of work in the new database setup script. Is known to correctly build the tables.

---
+++ NewScheme.sql
@@ -0,0 +1,73 @@
+
+CREATE SCHEMA IF NOT EXISTS `agora` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `agora` ;
+SET storage_engine=INNODB;
+
+DROP TABLE IF EXISTS `agora`.`nodes`;
+DROP TABLE IF EXISTS `agora`.`maps`;
+DROP TABLE IF EXISTS `agora`.`users`;
+
+-- -----------------------------------------------------
+-- Table `agora`.`users`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `agora`.`users` (
+  `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `isDeleted` TINYINT(1)  NULL DEFAULT 1 ,
+  `firstName` VARCHAR(30) NULL ,
+  `lastName` VARCHAR(30) NULL ,
+  `userName` VARCHAR(32) NOT NULL ,
+  `password` VARCHAR(32) NOT NULL ,
+  `email` VARCHAR(50) NOT NULL ,
+  `url` VARCHAR(50) NULL ,
+  `userLevel` TINYINT(1) UNSIGNED NULL DEFAULT 1 COMMENT '1 = Standard user\n9 = Administrator' ,
+  `createdDate` DATETIME NULL ,
+  `lastLogin` DATETIME NULL ,	
+  PRIMARY KEY (`user_id`) ,
+  UNIQUE INDEX `userName_UNIQUE` (`userName` ASC) ,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC));
+
+
+-- -----------------------------------------------------
+-- Table `agora`.`maps`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `agora`.`maps` (
+  `map_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `user_id` INT UNSIGNED NULL ,
+  `title` VARCHAR(100) NULL ,
+  `description` VARCHAR(255) NULL ,
+  `createdDate` DATETIME NULL ,
+  `modifiedDate` DATETIME NULL ,
+  PRIMARY KEY (`map_id`) ,
+  INDEX `user_id` (`user_id` ASC) ,
+  FOREIGN KEY (`user_id`)
+    REFERENCES `agora`.`users` (`user_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+-- -------------------------------------------------------
+-- Table `agora`.`nodes`
+-- -------------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `agora`.`nodes` (
+  `node_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  `user_id` INT UNSIGNED NOT NULL ,
+  `map_id` INT UNSIGNED NULL , 
+  `node_type` INT UNSIGNED NOT NULL,
+  `createdDate` DATETIME NULL ,
+  `modifiedDate` DATETIME NULL ,
+  `x_coord` INT NOT NULL,
+  `y_coord` INT NOT NULL,
+  `width` INT NOT NULL,
+  `height` INT NOT NULL,
+  PRIMARY KEY (`node_id`) ,
+  INDEX `user_id` (`user_id` ASC) ,
+  INDEX `map_id` (`map_id` ASC) ,
+  FOREIGN KEY (`user_id` )
+	REFERENCES `agora`.`users` (`user_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  FOREIGN KEY (`map_id` )
+	REFERENCES `agora`.`maps` (`map_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+--node_type includes things like:
+-- "universal reason", "particular reason", "comment", "inference", etc.
