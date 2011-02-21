4691073f65bc5c7279ce99c7a77488da87849a46
JoshJGT <joshj777@gmail.com>
2011-02-21 09:25:24 -0500

    Remove needless backticks in the new Scheme

--- NewScheme.sql
+++ NewScheme.sql
@@ -1,132 +1,132 @@
 
-CREATE SCHEMA IF NOT EXISTS `agora` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `agora` ;
+CREATE SCHEMA IF NOT EXISTS connectionTypesagoraconnectionTypes DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE connectionTypesagoraconnectionTypes ;
 SET storage_engine=INNODB;
 
-DROP TABLE IF EXISTS `agora`.`connections`;
-DROP TABLE IF EXISTS `agora`.`arguments`;
-DROP TABLE IF EXISTS `agora`.`connection_types`;
-DROP TABLE IF EXISTS `agora`.`nodetext`;
-DROP TABLE IF EXISTS `agora`.`textboxes`;
-DROP TABLE IF EXISTS `agora`.`nodes`;
-DROP TABLE IF EXISTS `agora`.`nodetypes`;
-DROP TABLE IF EXISTS `agora`.`maps`;
-DROP TABLE IF EXISTS `agora`.`users`;
+DROP TABLE IF EXISTS connectionTypesagoraconnectionTypes.connectionTypesconnectionsconnectionTypes;
+DROP TABLE IF EXISTS connectionTypesagoraconnectionTypes.connectionTypesargumentsconnectionTypes;
+DROP TABLE IF EXISTS connectionTypesagoraconnectionTypes.connectionTypesconnection_typesconnectionTypes;
+DROP TABLE IF EXISTS connectionTypesagoraconnectionTypes.connectionTypesnodetextconnectionTypes;
+DROP TABLE IF EXISTS connectionTypesagoraconnectionTypes.connectionTypestextboxesconnectionTypes;
+DROP TABLE IF EXISTS connectionTypesagoraconnectionTypes.connectionTypesnodesconnectionTypes;
+DROP TABLE IF EXISTS connectionTypesagoraconnectionTypes.connectionTypesnodetypesconnectionTypes;
+DROP TABLE IF EXISTS connectionTypesagoraconnectionTypes.connectionTypesmapsconnectionTypes;
+DROP TABLE IF EXISTS connectionTypesagoraconnectionTypes.connectionTypesusersconnectionTypes;
 
 -- -----------------------------------------------------
--- Table `agora`.`users`
+-- Table connectionTypesagoraconnectionTypes.connectionTypesusersconnectionTypes
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `agora`.`users` (
-  `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `isDeleted` TINYINT(1)  NULL DEFAULT 1 ,
-  `firstName` VARCHAR(30) NULL ,
-  `lastName` VARCHAR(30) NULL ,
-  `userName` VARCHAR(32) NOT NULL ,
-  `password` VARCHAR(32) NOT NULL ,
-  `email` VARCHAR(255) NOT NULL ,
-  `url` VARCHAR(255) NULL ,
-  `userLevel` TINYINT(1) UNSIGNED NULL DEFAULT 1 COMMENT '1 = Standard user\n9 = Administrator' ,
-  `createdDate` DATETIME NULL ,
-  `lastLogin` DATETIME NULL ,	
-  PRIMARY KEY (`user_id`) ,
-  UNIQUE INDEX `userName_UNIQUE` (`userName` ASC) ,
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC));
+CREATE  TABLE IF NOT EXISTS connectionTypesagoraconnectionTypes.connectionTypesusersconnectionTypes (
+  connectionTypesuser_idconnectionTypes INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  connectionTypesisDeletedconnectionTypes TINYINT(1)  NULL DEFAULT 1 ,
+  connectionTypesfirstNameconnectionTypes VARCHAR(30) NULL ,
+  connectionTypeslastNameconnectionTypes VARCHAR(30) NULL ,
+  connectionTypesuserNameconnectionTypes VARCHAR(32) NOT NULL ,
+  connectionTypespasswordconnectionTypes VARCHAR(32) NOT NULL ,
+  connectionTypesemailconnectionTypes VARCHAR(255) NOT NULL ,
+  connectionTypesurlconnectionTypes VARCHAR(255) NULL ,
+  connectionTypesuserLevelconnectionTypes TINYINT(1) UNSIGNED NULL DEFAULT 1 COMMENT '1 = Standard user\n9 = Administrator' ,
+  connectionTypescreatedDateconnectionTypes DATETIME NULL ,
+  connectionTypeslastLoginconnectionTypes DATETIME NULL ,	
+  PRIMARY KEY (connectionTypesuser_idconnectionTypes) ,
+  UNIQUE INDEX connectionTypesuserName_UNIQUEconnectionTypes (connectionTypesuserNameconnectionTypes ASC) ,
+  UNIQUE INDEX connectionTypesemail_UNIQUEconnectionTypes (connectionTypesemailconnectionTypes ASC));
 
 
 -- -----------------------------------------------------
--- Table `agora`.`maps`
+-- Table connectionTypesagoraconnectionTypes.connectionTypesmapsconnectionTypes
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `agora`.`maps` (
-  `map_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `user_id` INT UNSIGNED NULL ,
-  `title` VARCHAR(100) NULL ,
-  `description` VARCHAR(255) NULL ,
-  `createdDate` DATETIME NULL ,
-  `modifiedDate` DATETIME NULL ,
-  PRIMARY KEY (`map_id`) ,
-  INDEX `user_id` (`user_id` ASC) ,
-  FOREIGN KEY (`user_id`)
-    REFERENCES `agora`.`users` (`user_id`)
+CREATE  TABLE IF NOT EXISTS connectionTypesagoraconnectionTypes.connectionTypesmapsconnectionTypes (
+  connectionTypesmap_idconnectionTypes INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  connectionTypesuser_idconnectionTypes INT UNSIGNED NULL ,
+  connectionTypestitleconnectionTypes VARCHAR(100) NULL ,
+  connectionTypesdescriptionconnectionTypes VARCHAR(255) NULL ,
+  connectionTypescreatedDateconnectionTypes DATETIME NULL ,
+  connectionTypesmodifiedDateconnectionTypes DATETIME NULL ,
+  PRIMARY KEY (connectionTypesmap_idconnectionTypes) ,
+  INDEX connectionTypesuser_idconnectionTypes (connectionTypesuser_idconnectionTypes ASC) ,
+  FOREIGN KEY (connectionTypesuser_idconnectionTypes)
+    REFERENCES connectionTypesagoraconnectionTypes.connectionTypesusersconnectionTypes (connectionTypesuser_idconnectionTypes)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 -- -------------------------------------------------------
--- Table `agora`.`node_types`
+-- Table connectionTypesagoraconnectionTypes.connectionTypesnode_typesconnectionTypes
 -- -------------------------------------------------------
-CREATE TABLE IF NOT EXISTS `agora`.`node_types` (
-  `nodetype_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(30) NOT NULL,
-  PRIMARY KEY (`nodetype_id`));
+CREATE TABLE IF NOT EXISTS connectionTypesagoraconnectionTypes.connectionTypesnode_typesconnectionTypes (
+  connectionTypesnodetype_idconnectionTypes INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  connectionTypesnameconnectionTypes VARCHAR(30) NOT NULL,
+  PRIMARY KEY (connectionTypesnodetype_idconnectionTypes));
 
 INSERT INTO node_types (name) VALUES ("Standard"), ("Inference"), ("Objection"), ("Amendment"), ("Comment");
 -- -------------------------------------------------------
--- Table `agora`.`nodes`
+-- Table connectionTypesagoraconnectionTypes.connectionTypesnodesconnectionTypes
 -- -------------------------------------------------------
-CREATE TABLE IF NOT EXISTS `agora`.`nodes` (
-  `node_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `user_id` INT UNSIGNED NOT NULL ,
-  `map_id` INT UNSIGNED NULL , 
-  `node_type` INT UNSIGNED NOT NULL,
-  `createdDate` DATETIME NULL ,
-  `modifiedDate` DATETIME NULL ,
-  `x_coord` INT NULL,
-  `y_coord` INT NULL,
-  `width` INT NULL,
-  `height` INT NULL,
-  PRIMARY KEY (`node_id`) ,
-  INDEX `user_id` (`user_id` ASC) ,
-  INDEX `map_id` (`map_id` ASC) ,
-  INDEX `node_type` (`node_type` ASC) ,
-  FOREIGN KEY (`user_id`)
-	REFERENCES `agora`.`users` (`user_id`)
+CREATE TABLE IF NOT EXISTS connectionTypesagoraconnectionTypes.connectionTypesnodesconnectionTypes (
+  connectionTypesnode_idconnectionTypes INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  connectionTypesuser_idconnectionTypes INT UNSIGNED NOT NULL ,
+  connectionTypesmap_idconnectionTypes INT UNSIGNED NULL , 
+  connectionTypesnode_typeconnectionTypes INT UNSIGNED NOT NULL,
+  connectionTypescreatedDateconnectionTypes DATETIME NULL ,
+  connectionTypesmodifiedDateconnectionTypes DATETIME NULL ,
+  connectionTypesx_coordconnectionTypes INT NULL,
+  connectionTypesy_coordconnectionTypes INT NULL,
+  connectionTypeswidthconnectionTypes INT NULL,
+  connectionTypesheightconnectionTypes INT NULL,
+  PRIMARY KEY (connectionTypesnode_idconnectionTypes) ,
+  INDEX connectionTypesuser_idconnectionTypes (connectionTypesuser_idconnectionTypes ASC) ,
+  INDEX connectionTypesmap_idconnectionTypes (connectionTypesmap_idconnectionTypes ASC) ,
+  INDEX connectionTypesnode_typeconnectionTypes (connectionTypesnode_typeconnectionTypes ASC) ,
+  FOREIGN KEY (connectionTypesuser_idconnectionTypes)
+	REFERENCES connectionTypesagoraconnectionTypes.connectionTypesusersconnectionTypes (connectionTypesuser_idconnectionTypes)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  FOREIGN KEY (`map_id`)
-	REFERENCES `agora`.`maps` (`map_id`)
+  FOREIGN KEY (connectionTypesmap_idconnectionTypes)
+	REFERENCES connectionTypesagoraconnectionTypes.connectionTypesmapsconnectionTypes (connectionTypesmap_idconnectionTypes)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  FOREIGN KEY(`node_type`)
-    REFERENCES `agora`.`node_types` (`nodetype_id`)
+  FOREIGN KEY(connectionTypesnode_typeconnectionTypes)
+    REFERENCES connectionTypesagoraconnectionTypes.connectionTypesnode_typesconnectionTypes (connectionTypesnodetype_idconnectionTypes)
 	ON DELETE NO ACTION
 	ON UPDATE NO ACTION);
 
 -- -----------------------------------------------------
--- Table `agora`.`textboxes`
+-- Table connectionTypesagoraconnectionTypes.connectionTypestextboxesconnectionTypes
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `agora`.`textboxes` (
-	`textbox_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`text` TEXT,
-	PRIMARY KEY (`textbox_id`));
+CREATE TABLE IF NOT EXISTS connectionTypesagoraconnectionTypes.connectionTypestextboxesconnectionTypes (
+	connectionTypestextbox_idconnectionTypes INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	connectionTypestextconnectionTypes TEXT,
+	PRIMARY KEY (connectionTypestextbox_idconnectionTypes));
 
 -- -----------------------------------------------------
--- Table `agora`.`nodetext`
+-- Table connectionTypesagoraconnectionTypes.connectionTypesnodetextconnectionTypes
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `agora`.`nodetext` (
-	`nodetext_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`node_id` INT UNSIGNED NOT NULL,
-	`textbox_id` INT UNSIGNED NOT NULL,
-	`position` INT UNSIGNED NOT NULL,
-	PRIMARY KEY (`nodetext_id`),
-	INDEX `node_id` (`node_id` ASC),
-	INDEX `textbox_id` (`textbox_id` ASC),
-	FOREIGN KEY(`node_id`)
-	  REFERENCES `agora`.`nodes` (`node_id`)
+CREATE TABLE IF NOT EXISTS connectionTypesagoraconnectionTypes.connectionTypesnodetextconnectionTypes (
+	connectionTypesnodetext_idconnectionTypes INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	connectionTypesnode_idconnectionTypes INT UNSIGNED NOT NULL,
+	connectionTypestextbox_idconnectionTypes INT UNSIGNED NOT NULL,
+	connectionTypespositionconnectionTypes INT UNSIGNED NOT NULL,
+	PRIMARY KEY (connectionTypesnodetext_idconnectionTypes),
+	INDEX connectionTypesnode_idconnectionTypes (connectionTypesnode_idconnectionTypes ASC),
+	INDEX connectionTypestextbox_idconnectionTypes (connectionTypestextbox_idconnectionTypes ASC),
+	FOREIGN KEY(connectionTypesnode_idconnectionTypes)
+	  REFERENCES connectionTypesagoraconnectionTypes.connectionTypesnodesconnectionTypes (connectionTypesnode_idconnectionTypes)
 	  ON DELETE NO ACTION
 	  ON UPDATE NO ACTION,
-	FOREIGN KEY(`textbox_id`)
-	  REFERENCES `agora`.`textboxes` (`textbox_id`)
+	FOREIGN KEY(connectionTypestextbox_idconnectionTypes)
+	  REFERENCES connectionTypesagoraconnectionTypes.connectionTypestextboxesconnectionTypes (connectionTypestextbox_idconnectionTypes)
 	  ON DELETE NO ACTION
 	  ON UPDATE NO ACTION);
 
 	
 -- -----------------------------------------------------
--- Table `agora`.`connection_types`
+-- Table connectionTypesagoraconnectionTypes.connectionTypesconnection_typesconnectionTypes
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `agora`.`connection_types` (
-  `type_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(60) NOT NULL ,
-  `description` VARCHAR(255) NULL,
-  PRIMARY KEY (`type_id`));
+CREATE  TABLE IF NOT EXISTS connectionTypesagoraconnectionTypes.connectionTypesconnection_typesconnectionTypes (
+  connectionTypestype_idconnectionTypes INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  connectionTypesnameconnectionTypes VARCHAR(60) NOT NULL ,
+  connectionTypesdescriptionconnectionTypes VARCHAR(255) NULL,
+  PRIMARY KEY (connectionTypestype_idconnectionTypes));
   
 INSERT INTO connection_types (name, description) VALUES 
 	("Commentary", "A comment on another node"), ("Objection", "An objection to another node"), ("Refutation", "A refutation of another node"), ("Amendment", "Proposed amendment for another node");
@@ -134,47 +134,47 @@ INSERT INTO connection_types (name, description) VALUES
 INSERT INTO connection_types(name, description) VALUES ("MPtherefore", "Modus Ponens - Therefore phrasing, in English.");
 
 -- -----------------------------------------------------
--- Table `agora`.`arguments`
+-- Table connectionTypesagoraconnectionTypes.connectionTypesargumentsconnectionTypes
 -- -----------------------------------------------------
 
-CREATE  TABLE IF NOT EXISTS `agora`.`arguments` (
-  `argument_id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `map_id` INT UNSIGNED NULL,
-  `node_id` INT UNSIGNED NULL ,
-  `type_id` INT UNSIGNED NULL ,
-  PRIMARY KEY (`argument_id`) ,
-  INDEX `map_id` (`map_id` ASC) ,
-  INDEX `node_id` (`node_id` ASC) ,
-  INDEX `type_id` (`type_id` ASC) ,
-  FOREIGN KEY (`map_id`)
-    REFERENCES `agora`.`maps` (`map_id`)
+CREATE  TABLE IF NOT EXISTS connectionTypesagoraconnectionTypes.connectionTypesargumentsconnectionTypes (
+  connectionTypesargument_idconnectionTypes INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+  connectionTypesmap_idconnectionTypes INT UNSIGNED NULL,
+  connectionTypesnode_idconnectionTypes INT UNSIGNED NULL ,
+  connectionTypestype_idconnectionTypes INT UNSIGNED NULL ,
+  PRIMARY KEY (connectionTypesargument_idconnectionTypes) ,
+  INDEX connectionTypesmap_idconnectionTypes (connectionTypesmap_idconnectionTypes ASC) ,
+  INDEX connectionTypesnode_idconnectionTypes (connectionTypesnode_idconnectionTypes ASC) ,
+  INDEX connectionTypestype_idconnectionTypes (connectionTypestype_idconnectionTypes ASC) ,
+  FOREIGN KEY (connectionTypesmap_idconnectionTypes)
+    REFERENCES connectionTypesagoraconnectionTypes.connectionTypesmapsconnectionTypes (connectionTypesmap_idconnectionTypes)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  FOREIGN KEY (`node_id`)
-    REFERENCES `agora`.`nodes` (`node_id`)
+  FOREIGN KEY (connectionTypesnode_idconnectionTypes)
+    REFERENCES connectionTypesagoraconnectionTypes.connectionTypesnodesconnectionTypes (connectionTypesnode_idconnectionTypes)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  FOREIGN KEY (`type_id`)
-    REFERENCES `agora`.`connection_types` (`type_id`)
+  FOREIGN KEY (connectionTypestype_idconnectionTypes)
+    REFERENCES connectionTypesagoraconnectionTypes.connectionTypesconnection_typesconnectionTypes (connectionTypestype_idconnectionTypes)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 -- The node_id is the claim.
 
 -- -----------------------------------------------------
--- Table `agora`.`connections`
+-- Table connectionTypesagoraconnectionTypes.connectionTypesconnectionsconnectionTypes
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `agora`.`connections` (
-	`connection_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`argument_id` INT UNSIGNED NOT NULL,
-	`node_id` INT UNSIGNED NOT NULL,
-	PRIMARY KEY (`connection_id`),
-	INDEX `argument_id` (`argument_id` ASC),
-	INDEX `node_id` (`node_id` ASC),
-	FOREIGN KEY (`argument_id`)
-	  REFERENCES `agora`.`arguments` (`argument_id`)
+CREATE TABLE IF NOT EXISTS connectionTypesagoraconnectionTypes.connectionTypesconnectionsconnectionTypes (
+	connectionTypesconnection_idconnectionTypes INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	connectionTypesargument_idconnectionTypes INT UNSIGNED NOT NULL,
+	connectionTypesnode_idconnectionTypes INT UNSIGNED NOT NULL,
+	PRIMARY KEY (connectionTypesconnection_idconnectionTypes),
+	INDEX connectionTypesargument_idconnectionTypes (connectionTypesargument_idconnectionTypes ASC),
+	INDEX connectionTypesnode_idconnectionTypes (connectionTypesnode_idconnectionTypes ASC),
+	FOREIGN KEY (connectionTypesargument_idconnectionTypes)
+	  REFERENCES connectionTypesagoraconnectionTypes.connectionTypesargumentsconnectionTypes (connectionTypesargument_idconnectionTypes)
 	  ON DELETE NO ACTION
 	  ON UPDATE NO ACTION,
-	FOREIGN KEY (`node_id`)
-	  REFERENCES `agora`.`nodes` (`node_id`)
+	FOREIGN KEY (connectionTypesnode_idconnectionTypes)
+	  REFERENCES connectionTypesagoraconnectionTypes.connectionTypesnodesconnectionTypes (connectionTypesnode_idconnectionTypes)
 	  ON DELETE NO ACTION
 	  ON UPDATE NO ACTION);
