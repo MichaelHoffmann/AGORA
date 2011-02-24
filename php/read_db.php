83dc69742bc136cbd7d3ebaa1f68f840aa1733db
JoshJGT <joshj777@gmail.com>
2011-02-24 15:18:31 -0500

    Modified the database reading script to do the same thing in XML. This is not the proper XML layout, but is progress in the direction of the correct XML layout.

--- php/read_db.php
+++ php/read_db.php
@@ -1,15 +1,18 @@
 <?php
-
 	$linkID = mysql_connect("localhost", "root", "") or die ("Could not connect to database!");
 	mysql_select_db("agora", $linkID) or die ("Could not find database");
 	$query = "SELECT * FROM nodes NATURAL JOIN nodetext NATURAL JOIN textboxes NATURAL JOIN node_types";
 	$resultID = mysql_query($query, $linkID) or die("Data not found."); 
-	echo("<FONT face=\"Gill Sans Mono\">");
-	echo("&nbsp;&nbsp;ID |&nbsp;&nbsp;&nbsp;Type&nbsp;&nbsp;&nbsp;| Text<BR>");
+
+	header("Content-type: text/xml");
+	$xmlstr = "<?xml version='1.0' ?>\n<book></book>";
+	$xml = new SimpleXMLElement($xmlstr);
 	for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
 		$row = mysql_fetch_assoc($resultID);
-		echo("&nbsp;&nbsp;&nbsp;" . $row['node_id'] . " | " . $row['type'] . " | " . $row['text']);
-		echo("<BR>-------------------------</BR>");
+		$node = $xml->addChild("node");
+		$node->addAttribute("ID", $row['node_id']);
+		$node->addAttribute("type", $row['type']);
+		$node->addAttribute("text", $row['text']);
 	}
-	echo("</FONT>");
+	print($xml->asXML());
 ?>
