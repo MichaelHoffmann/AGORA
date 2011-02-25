d6d7dbc0ec5ec88d3a8989b0baa7605ca7e19f73
joshjgt <Joshua.Justice@gatech.edu>
2011-02-25 14:00:05 -0500

    Made changes to get it working

--- load_map.php
+++ load_map.php
@@ -2,7 +2,16 @@
 	$linkID = mysql_connect("localhost", "root", "") or die ("Could not connect to database!");
 	mysql_select_db("agora", $linkID) or die ("Could not find database");
 	$mapID = 1; //TODO: read this from input somehow.
-	$whereclause = mysql_real_escape_string($mapID);
-	$query = "SELECT * FROM maps " . $whereclause; //WHERE mapID == something passed in....
+	$whereclause = mysql_real_escape_string("WHERE map_id = $mapID");
+	$query = "SELECT * FROM maps " . $whereclause ; //WHERE mapID == something passed in....	
+	$resultID = mysql_query($query, $linkID) or die("Data not found."); 
 	
+	header("Content-type: text/xml");
+	$xmlstr = "<?xml version='1.0' ?>\n<map></map>";
+	$xml = new SimpleXMLElement($xmlstr);
+	$row = mysql_fetch_assoc($resultID);
+	$xml->addAttribute("id", $row['map_id']);
+	$xml->addAttribute("creator", $row['user_id']);
+	
+	print($xml->asXML());
 ?>
