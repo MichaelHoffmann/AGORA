add916ed3529a44a4a31d638a1021327e8f07258
JoshJGT <joshj777@gmail.com>
2011-03-02 13:47:51 -0500

    Define a function and move it to the proper folder

---
+++ php/load_map.php
@@ -0,0 +1,21 @@
+<?php
+
+	function get_map($mapID){
+
+		$linkID = mysql_connect("localhost", "root", "") or die ("Could not connect to database!");
+		mysql_select_db("agora", $linkID) or die ("Could not find database");
+		$mapID = 1; //TODO: read this from input somehow.
+		$whereclause = mysql_real_escape_string("WHERE map_id = $mapID");
+		$query = "SELECT * FROM maps " . $whereclause ; //WHERE mapID == something passed in....	
+		$resultID = mysql_query($query, $linkID) or die("Data not found."); 
+		
+		header("Content-type: text/xml");
+		$xmlstr = "<?xml version='1.0' ?>\n<map></map>";
+		$xml = new SimpleXMLElement($xmlstr);
+		$row = mysql_fetch_assoc($resultID);
+		$xml->addAttribute("id", $row['map_id']);
+		$xml->addAttribute("creator", $row['user_id']);
+		return $xml;
+	}
+	print($xml->asXML());
+?>
