ef22d1eaf2d97221dd7eeebba2e3095cf1cbe974
JoshJGT <joshj777@gmail.com>
2011-03-02 15:09:57 -0500

    Got nodetext working. Guaranteed to be in the proper order, too.

--- php/load_map.php
+++ php/load_map.php
@@ -5,7 +5,7 @@
 		$linkID = mysql_connect("localhost", "root", "") or die ("Could not connect to database!");
 		mysql_select_db("agora", $linkID) or die ("Could not find database");
 		$whereclause = mysql_real_escape_string("WHERE map_id = $mapID");
-		$query = "SELECT * FROM maps " . $whereclause ; //WHERE mapID == something passed in....	
+		$query = "SELECT * FROM maps " . $whereclause ;
 		$resultID = mysql_query($query, $linkID) or die("Data not found."); 
 		
 		header("Content-type: text/xml");
@@ -15,8 +15,8 @@
 		$xml->addAttribute("id", $row['map_id']);
 		$xml->addAttribute("creator", $row['user_id']);
 		
-		$query = "SELECT * FROM textboxes " . $whereclause ; //WHERE mapID == something passed in....	
-		$resultID = mysql_query($query, $linkID) or die("Data not found."); 
+		$query = "SELECT * FROM textboxes " . $whereclause ; 
+		$resultID = mysql_query($query, $linkID) or die("Data not found in textbox lookup."); 
 		for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
 			$row = mysql_fetch_assoc($resultID);
 			$textbox = $xml->addChild("textbox");
@@ -24,7 +24,23 @@
 			$textbox->addAttribute("text", $row['text']);
 		}
 		
+		$query = "SELECT * FROM nodes " . $whereclause;
 		
+		$resultID = mysql_query($query, $linkID) or die("Data not found in node lookup."); 
+		for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
+			$row = mysql_fetch_assoc($resultID);
+			$node_id = $row['node_id'];
+			$node = $xml->addChild("node");
+			$node->addAttribute("ID", $row['node_id']);
+			$node->addAttribute("Author", $row['user_id']);
+			$innerQuery="SELECT * from nodetext WHERE node_id=$node_id ORDER BY position ASC";
+			$resultID2 = mysql_query($innerQuery, $linkID) or die("Data not found in nodetext lookup."); 
+			for($y=0; $y<mysql_num_rows($resultID2); $y++){
+				$nodetext = $node->addChild("nodetext");
+				$innerRow=mysql_fetch_assoc($resultID2);
+				$nodetext->addAttribute("ID", $innerRow['textbox_id']);
+			}			
+		}
 		
 		return $xml;
 	}
