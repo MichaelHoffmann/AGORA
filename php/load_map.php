e7cf7a05e3da0b73c8db9520471348776c6a61ac
JoshJGT <joshj777@gmail.com>
2011-03-03 14:21:27 -0500

    No need for a proper schema document.

--- php/load_map.php
+++ php/load_map.php
@@ -6,16 +6,16 @@
 		$linkID = mysql_connect("localhost", "root", "") or die ("Could not connect to database!");
 		mysql_select_db("agora", $linkID) or die ("Could not find database");
 		$whereclause = mysql_real_escape_string("WHERE map_id = $mapID");
-		$query = "SELECT * FROM maps " . $whereclause ;
+		$query = "SELECT * FROM maps NATURAL JOIN users " . $whereclause ;
 		$resultID = mysql_query($query, $linkID) or die("Data not found."); 
-		
 		//Set up the basics of the XML.
 		header("Content-type: text/xml");
 		$xmlstr = "<?xml version='1.0' ?>\n<map></map>";
 		$xml = new SimpleXMLElement($xmlstr);
 		$row = mysql_fetch_assoc($resultID);
 		$xml->addAttribute("id", $row['map_id']);
-		$xml->addAttribute("creator", $row['user_id']);
+		$xml->addAttribute("username", $row['username']);
+		
 		
 		// Textboxes are easy!
 		$query = "SELECT * FROM textboxes " . $whereclause ; 
@@ -35,10 +35,10 @@
 			$row = mysql_fetch_assoc($resultID);
 			$node_id = $row['node_id'];
 			$node = $xml->addChild("node");
-			$node->addAttribute("ID", $row['node_id']);
+			$node->addAttribute("ID", $node_id);
 			$node->addAttribute("Author", $row['user_id']);
 			//Have to do this instead of a proper join for the simple reason that we don't want to have multiple instances of the same <node>
-			$innerQuery="SELECT * from nodetext WHERE node_id=$node_id ORDER BY position ASC";
+			$innerQuery="SELECT * FROM nodetext WHERE node_id=$node_id ORDER BY position ASC";
 			$resultID2 = mysql_query($innerQuery, $linkID) or die("Data not found in nodetext lookup."); 
 			for($y=0; $y<mysql_num_rows($resultID2); $y++){
 				$nodetext = $node->addChild("nodetext");
@@ -47,8 +47,25 @@
 			}			
 		}
 
-		
-		
+		// Connections will take a lot more work.
+		$query = "SELECT * FROM arguments NATURAL JOIN connection_types " . $whereclause;
+		$resultID = mysql_query($query, $linkID) or die("Data not found in arg lookup.");
+		for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
+			$row = mysql_fetch_assoc($resultID);
+			$arg_id=$row['argument_id'];
+			$connection = $xml->addChild("connection");
+			$connection->addAttribute("argID", $arg_id);
+			$connection->addAttribute("type", $row['conn_name']);
+			$connection->addAttribute("targetnode", $row['node_id']);
+			//Set up the inner query to find the source nodes
+			$innerQuery="SELECT * FROM connections WHERE argument_id=$arg_id";
+			$resultID2 = mysql_query($innerQuery, $linkID) or die("Data not found in connection lookup");
+			for($y=0; $y<mysql_num_rows($resultID2); $y++){
+				$sourcenode = $connection->addChild("sourcenode");
+				$innerRow=mysql_fetch_assoc($resultID2);
+				$sourcenode->addAttribute("nodeID", $innerRow['node_id']);
+			}	
+		}
 		
 		
 		return $xml;
