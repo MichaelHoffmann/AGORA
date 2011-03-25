85c64cd727c2fb89f3c569b57bff1867f78515f1
JoshJGT <joshj777@gmail.com>
2011-03-25 14:33:25 -0400

    Consistency fix

--- php/load_map.php
+++ php/load_map.php
@@ -6,7 +6,7 @@
 		mysql_select_db("agora", $linkID) or die ("Could not find database");
 		$whereclause = mysql_real_escape_string("$mapID");
 		$timeclause = mysql_real_escape_string("$timestamp");
-		$query = "SELECT * FROM maps NATURAL JOIN users WHERE map_id = " . $whereclause;
+		$query = "SELECT * FROM maps NATURAL JOIN users WHERE map_id = $whereclause";
 		$resultID = mysql_query($query, $linkID) or die("Data not found."); 
 		//Set up the basics of the XML.
 		header("Content-type: text/xml");
