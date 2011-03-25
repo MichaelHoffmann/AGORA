084a643458e03fed878e9f9c751438e6b9ba930a
JoshJGT <joshj777@gmail.com>
2011-03-25 14:49:35 -0400

    Looks like I have timestamps working. This means we should have partial updates working later on... of course, client's stuck figuring out whether it is an add or a modify at this point.

--- php/load_map.php
+++ php/load_map.php
@@ -16,60 +16,67 @@
 		$xml->addAttribute("id", $row['map_id']);
 		$xml->addAttribute("username", $row['username']);
 		
-		$now = mysql_query("SELECT NOW()", $linkID) or die ("Could not get timestamp from server.");
+		$timeID = mysql_query("SELECT NOW()", $linkID) or die ("Could not get timestamp from server.");
+		$timerow = mysql_fetch_assoc($timeID);
+		$now = $timerow['NOW()'];
 		$xml->addAttribute("timestamp", "$now");
 		
 		// Textboxes are easy!
-		$query = "SELECT * FROM textboxes WHERE map_id = $whereclause"; 
-		$resultID = mysql_query($query, $linkID) or die("Data not found in textbox lookup."); 
-		for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
-			$row = mysql_fetch_assoc($resultID);
-			$textbox = $xml->addChild("textbox");
-			$textbox->addAttribute("ID", $row['textbox_id']);
-			$textbox->addAttribute("text", $row['text']);
+		$query = "SELECT * FROM textboxes WHERE map_id = $whereclause AND modified_time>$timeclause"; 
+		$resultID = mysql_query($query, $linkID); 
+		if($resultID){
+			for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
+				$row = mysql_fetch_assoc($resultID);
+				$textbox = $xml->addChild("textbox");
+				$textbox->addAttribute("ID", $row['textbox_id']);
+				$textbox->addAttribute("text", $row['text']);
+			}
 		}
 
 
 		// Nodes take a bit more work.
-		$query = "SELECT * FROM nodes NATURAL JOIN node_types WHERE map_id = $whereclause";
-		$resultID = mysql_query($query, $linkID) or die("Data not found in node lookup."); 
-		for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
-			$row = mysql_fetch_assoc($resultID);
-			$node_id = $row['node_id'];
-			$node = $xml->addChild("node");
-			$node->addAttribute("ID", $node_id);
-			$node->addAttribute("Type", $row['type']);
-			$node->addAttribute("Author", $row['user_id']);
-			//Have to do this instead of a proper join for the simple reason that we don't want to have multiple instances of the same <node>
-			$innerQuery="SELECT * FROM nodetext WHERE node_id=$node_id ORDER BY position ASC";
-			$resultID2 = mysql_query($innerQuery, $linkID) or die("Data not found in nodetext lookup."); 
-			for($y=0; $y<mysql_num_rows($resultID2); $y++){
-				$nodetext = $node->addChild("nodetext");
-				$innerRow=mysql_fetch_assoc($resultID2);
-				$nodetext->addAttribute("ID", $innerRow['textbox_id']);
-			}			
+		$query = "SELECT * FROM nodes NATURAL JOIN node_types WHERE map_id = $whereclause AND modified_time>$timeclause";
+		$resultID = mysql_query($query, $linkID); 
+		if($resultID){
+			for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
+				$row = mysql_fetch_assoc($resultID);
+				$node_id = $row['node_id'];
+				$node = $xml->addChild("node");
+				$node->addAttribute("ID", $node_id);
+				$node->addAttribute("Type", $row['type']);
+				$node->addAttribute("Author", $row['user_id']);
+				//Have to do this instead of a proper join for the simple reason that we don't want to have multiple instances of the same <node>
+				$innerQuery="SELECT * FROM nodetext WHERE node_id=$node_id ORDER BY position ASC";
+				$resultID2 = mysql_query($innerQuery, $linkID) or die("Data not found in nodetext lookup."); 
+				for($y=0; $y<mysql_num_rows($resultID2); $y++){
+					$nodetext = $node->addChild("nodetext");
+					$innerRow=mysql_fetch_assoc($resultID2);
+					$nodetext->addAttribute("ID", $innerRow['textbox_id']);
+				}			
+			}
 		}
 
 		// Connections will take a lot more work.
-		$query = "SELECT * FROM arguments NATURAL JOIN connection_types WHERE map_id = $whereclause";
-		$resultID = mysql_query($query, $linkID) or die("Data not found in arg lookup.");
-		for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
-			$row = mysql_fetch_assoc($resultID);
-			$arg_id=$row['argument_id'];
-			$connection = $xml->addChild("connection");
-			$connection->addAttribute("argID", $arg_id);
-			$connection->addAttribute("type", $row['conn_name']);
-			$connection->addAttribute("targetnode", $row['node_id']);
-			//Set up the inner query to find the source nodes
-			$innerQuery="SELECT * FROM connections WHERE argument_id=$arg_id";
-			$resultID2 = mysql_query($innerQuery, $linkID) or die("Data not found in connection lookup");
-			for($y=0; $y<mysql_num_rows($resultID2); $y++){
-				$sourcenode = $connection->addChild("sourcenode");
-				$innerRow=mysql_fetch_assoc($resultID2);
-				$sourcenode->addAttribute("nodeID", $innerRow['node_id']);
-			}	
+		$query = "SELECT * FROM arguments NATURAL JOIN connection_types WHERE map_id = $whereclause AND modified_time>$timeclause";
+		$resultID = mysql_query($query, $linkID);
+		if($resultID){
+			for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
+				$row = mysql_fetch_assoc($resultID);
+				$arg_id=$row['argument_id'];
+				$connection = $xml->addChild("connection");
+				$connection->addAttribute("argID", $arg_id);
+				$connection->addAttribute("type", $row['conn_name']);
+				$connection->addAttribute("targetnode", $row['node_id']);
+				//Set up the inner query to find the source nodes
+				$innerQuery="SELECT * FROM connections WHERE argument_id=$arg_id";
+				$resultID2 = mysql_query($innerQuery, $linkID) or die("Data not found in connection lookup");
+				for($y=0; $y<mysql_num_rows($resultID2); $y++){
+					$sourcenode = $connection->addChild("sourcenode");
+					$innerRow=mysql_fetch_assoc($resultID2);
+					$sourcenode->addAttribute("nodeID", $innerRow['node_id']);
+				}	
+			}
 		}
-		
 		return $xml;
 	}
 	$map_id = $_REQUEST['map_id'];  //TODO: Change this back to a GET when all testing is done.
