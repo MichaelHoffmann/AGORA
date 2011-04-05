0da6fd909343f8ee5dc05dcdca3530267f05a4de
JoshJGT <joshj777@gmail.com>
2011-04-05 13:52:00 -0400

    Set up file structure

---
+++ php/insert.php
@@ -0,0 +1,17 @@
+<?php
+	function insert($map_id)
+	{
+		//Standard SQL connection stuff
+		$linkID = mysql_connect("localhost", "root", "") or die ("Could not connect to database!");
+		mysql_select_db("agora", $linkID) or die ("Could not find database");
+		$whereclause = mysql_real_escape_string("$mapID");
+		
+		//Set up the basics of the XML.
+		header("Content-type: text/xml");
+		$xmlstr = "<?xml version='1.0' ?>\n<map></map>";
+		$xml = new SimpleXMLElement($xmlstr);
+	}
+	$map_id = $_REQUEST['map_id'];  //TODO: Change this back to a GET when all testing is done.
+	$xml = insert($map_id); 
+	print($xml->asXML());
+?>
