28edfb3c2531fcdde6a69a209503551ed7eb0b18
JoshJGT <joshj777@gmail.com>
2011-04-05 14:37:09 -0400

    Define our plan of action for the code

--- php/insert.php
+++ php/insert.php
@@ -1,18 +1,33 @@
 <?php
-	function insert($map_id)
+	function insert($xml)
 	{
 		//Standard SQL connection stuff
 		$linkID = mysql_connect("localhost", "root", "") or die ("Could not connect to database!");
 		mysql_select_db("agora", $linkID) or die ("Could not find database");
-		$whereclause = mysql_real_escape_string("$mapID");
 		
-		//Set up the basics of the XML.
+		//Dig the Map ID out of the XML
+		
+		//Check to see if the map already exists
+		//$mapClause = mysql_real_escape_string("$mapID");
+		
+		//If not, create it!
+		
+		//Validate nodes
+		//Validate textboxes
+		//Validate nodetext
+		//Validate arguments
+		//Validate connections
+		
+		
+		//Insert/update everything
+		
+		//Set up the basics of the output XML.
 		header("Content-type: text/xml");
 		$xmlstr = "<?xml version='1.0' ?>\n<map></map>";
-		$xml = new SimpleXMLElement($xmlstr);
+		$output = new SimpleXMLElement($xmlstr);
+		return $output;
 	}
-	$map_id = $_REQUEST['map_id'];  //TODO: Change this back to a GET when all testing is done.
 	$xml = $_REQUEST['xml']; //TODO: Change this back to a GET when all testing is done.
-	$output = insert($map_id); 
+	$output = insert($xml); 
 	print($output->asXML());
 ?>
