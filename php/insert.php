bec69a4ffba52bd580ed4b265ed84aebe1628b91
JoshJGT <joshj777@gmail.com>
2011-04-05 16:26:38 -0400

    Own-Map checking complete

--- php/insert.php
+++ php/insert.php
@@ -1,5 +1,5 @@
 <?php
-	function insert($xmlin)
+	function insert($xmlin, $userID)
 	{
 		//Standard SQL connection stuff
 		$linkID = mysql_connect("localhost", "root", "") or die ("Could not connect to database!");
@@ -7,10 +7,21 @@
 		
 		//Dig the Map ID out of the XML
 		$xml = new SimpleXMLElement($xmlin);
-		print($xml['id']);
+		$mapID = $xml['id'];
 		
 		//Check to see if the map already exists
-		//$mapClause = mysql_real_escape_string("$mapID");
+		$mapClause = mysql_real_escape_string("$mapID");
+		$query = "SELECT * FROM maps NATURAL JOIN users WHERE map_id = $mapClause";
+		$resultID = mysql_query($query, $linkID) or die("Data not found."); 
+		$row = mysql_fetch_assoc($resultID);
+		
+		//Check to see if this is the map author
+		//If so, $ownMap is set to true.
+		$author = $row['user_id'];
+		$ownMap = false;
+		if($author == $userID){
+			$ownMap=true;
+		}
 		
 		//If not, create it!
 		
@@ -36,6 +47,7 @@
 		*/
 	}
 	$xml = $_REQUEST['xml']; //TODO: Change this back to a GET when all testing is done.
-	$output = insert($xml); 
+	$userID = $_REQUEST['uid'];
+	$output = insert($xml, $userID); 
 	//print($output->asXML()); //TODO: turn this back on
 ?>
