0622ce6c546da29c69281f7a81218bdb0cdf776d
JoshJGT <joshj777@gmail.com>
2011-04-11 13:17:32 -0400

    Insertion now verifies the password is correct

--- php/insert.php
+++ php/insert.php
@@ -1,6 +1,29 @@
 <?php
+
+	function checkLogin($userID, $pass_hash)
+	{
+		//$linkID = mysql_connect("localhost", "root", "s3s@me123") or die ("Could not connect to database!");
+		$linkID = mysql_connect("localhost", "root", "") or die ("Could not connect to database!");
+		mysql_select_db("agora", $linkID) or die ("Could not find database");
+		$userclause = mysql_real_escape_string("$userID");
+		$passclause = mysql_real_escape_string("$pass_hash");
+		$query = "SELECT * FROM users WHERE user_ID='$userID' AND password='$passclause'";
+		$resultID = mysql_query($query, $linkID) or die("Data not found."); 
+		//$row = mysql_fetch_assoc($resultID);
+		if(mysql_num_rows($resultID)>0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
 	function insert($xmlin, $userID, $pass_hash)
 	{
+		if(!checkLogin($userID, $pass_hash)){
+			print "Incorrect login!";
+			return;
+		}
+	
 		//Standard SQL connection stuff
 		//$linkID = mysql_connect("localhost", "root", "s3s@me123") or die ("Could not connect to database!");
 		$linkID = mysql_connect("localhost", "root", "") or die ("Could not connect to database!");
@@ -69,7 +92,7 @@
 	}
 	$xml = $_REQUEST['xml']; //TODO: Change this back to a GET when all testing is done.
 	$userID = $_REQUEST['uid'];
-	$passhash = $_REQUEST['pass_hash'];
+	$pass_hash = $_REQUEST['pass_hash'];
 	$output = insert($xml, $userID, $pass_hash); 
 	//print($output->asXML()); //TODO: turn this back on
 ?>
