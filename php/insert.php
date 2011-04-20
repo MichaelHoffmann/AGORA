e60c4e38c152965039cc11d838b619b4c010f7cf
JoshJGT <joshj777@gmail.com>
2011-04-20 15:40:48 -0400

    Move checkLogin to another file

--- php/insert.php
+++ php/insert.php
@@ -1,18 +1,6 @@
 <?php
 
-	function checkLogin($userID, $pass_hash, $linkID)
-	{
-		$userclause = mysql_real_escape_string("$userID");
-		$passclause = mysql_real_escape_string("$pass_hash");
-		$query = "SELECT * FROM users WHERE user_ID='$userID' AND password='$passclause'";
-		$resultID = mysql_query($query, $linkID) or die("Data not found."); 
-		//$row = mysql_fetch_assoc($resultID);
-		if(mysql_num_rows($resultID)>0){
-			return true;
-		}else{
-			return false;
-		}
-	}
+	require 'checklogin.php';
 
 	function getLastInsert($linkID)
 	{
