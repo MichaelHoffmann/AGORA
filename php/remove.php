392e17a5dd5b43df69587c7ab328f4b1a3f91859
JoshJGT <joshj777@gmail.com>
2011-04-21 15:34:37 -0400

    I cannot believe how long it took me to realize that typo

--- php/remove.php
+++ php/remove.php
@@ -13,7 +13,7 @@
 			print "<BR>Now deleting....<BR>";
 			$dquery = "DELETE FROM nodes WHERE node_id=$nID";
 			print $dquery;
-			return mysql_query($query, $linkID);
+			return mysql_query($dquery, $linkID);
 		}else{
 			print "<BR>You are attempting to delete someone else's work or a nonexistent node. This is not permissible.";
 			return false;
