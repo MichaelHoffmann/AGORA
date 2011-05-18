f8c9bd916aad8b5dc70b5be78c77c5ba8dc8329b
Joshua Justice <Joshua.Justice@gatech.edu>
2011-05-18 14:55:56 -0400

    Looks like I have smart removal working... Not ready to overwrite the default remove without more testing though.

--- php/remove.php
+++ php/remove.php
@@ -62,8 +62,8 @@
 	{
 	//Standard SQL connection stuff
 		//$linkID = mysql_connect("localhost", "root", "s3s@me123") or die ("Could not connect to database!");
-		//$linkID = mysql_connect("localhost", "root", "") or die ("Could not connect to database!");
-		$linkID = mysql_connect("localhost", "root", "root") or die ("Could not connect to database!");
+		$linkID = mysql_connect("localhost", "root", "") or die ("Could not connect to database!");
+		//$linkID = mysql_connect("localhost", "root", "root") or die ("Could not connect to database!");
 		mysql_select_db("agora", $linkID) or die ("Could not find database");
 
 		if(!checkLogin($userID, $pass_hash, $linkID)){
