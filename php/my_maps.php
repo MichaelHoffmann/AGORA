a12948a79e1b99eef55f7ac96720fcbc8d801fb9
JoshJGT <joshj777@gmail.com>
2011-05-24 14:33:29 -0400

    My Maps feature successfully added

--- php/my_maps.php
+++ php/my_maps.php
@@ -1,16 +1,30 @@
 <?php
+	require 'checklogin.php';
 	require 'establish_link.php';
+	
 	/**
-	*	File for getting the map list.
+	*	File for getting the list of maps a specific user has made.
 	*/
-	$linkID= establishLink();
-	mysql_select_db("agora", $linkID) or die ("Could not find database");
-	$query = "SELECT * FROM maps INNER JOIN users ON users.user_id = maps.user_id";
-	$resultID = mysql_query($query, $linkID) or die("Data not found."); 
-
+	
+	$userID = mysql_real_escape_string($_REQUEST['uid']);
+	$pass_hash = mysql_real_escape_string($_REQUEST['pass_hash']);
+	
 	header("Content-type: text/xml");
 	$xmlstr = "<?xml version='1.0' ?>\n<list></list>";
 	$xml = new SimpleXMLElement($xmlstr);
+	
+	$linkID= establishLink();
+	mysql_select_db("agora", $linkID) or die ("Could not find database");
+	if(!checkLogin($userID, $pass_hash, $linkID)){
+		$fail = $xml->addChild("error");
+		$fail->addAttribute("text", "Login failed!");
+		return;
+	}
+	
+	$query = "SELECT * FROM maps INNER JOIN users ON users.user_id = maps.user_id WHERE maps.user_id=$userID";
+	$resultID = mysql_query($query, $linkID) or die("Data not found."); 
+
+
 	for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
 		$row = mysql_fetch_assoc($resultID);
 		$map = $xml->addChild("map");
