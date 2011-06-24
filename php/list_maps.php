b7b99130226aec154572e2653073aee480170609
Joshua Justice <Joshua.Justice@gatech.edu>
2011-06-24 11:08:17 -0400

    Error checking added for map deletion

--- php/list_maps.php
+++ php/list_maps.php
@@ -7,15 +7,14 @@
 	mysql_select_db("agora", $linkID) or die ("Could not find database");
 	$query = "SELECT * FROM maps INNER JOIN users ON users.user_id = maps.user_id  AND maps.is_deleted=0";
 	$resultID = mysql_query($query, $linkID) or die("Data not found."); 
+
+	if(mysql_num_rows($resultID)==0){
+		print "There are no maps in the list! Query was: $query";
+		return false;
+	}
 	header("Content-type: text/xml");
 	$xmlstr = "<?xml version='1.0' ?>\n<list></list>";
 	$xml = new SimpleXMLElement($xmlstr);
-	if(mysql_num_rows($resultID)==0){
-		$fail=$xml->addChild("error");
-		$fail->addAttribute("text", "There are no maps in the list! Query was: $uquery");
-		return false;
-	}
-
 	for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
 		$row = mysql_fetch_assoc($resultID);
 		if($row['is_deleted']){
--- php/load_map.php
+++ php/load_map.php
@@ -12,16 +12,15 @@
 		$timeclause = mysql_real_escape_string("$timestamp");
 		$query = "SELECT * FROM maps INNER JOIN users ON users.user_id = maps.user_id WHERE map_id = $whereclause AND maps.is_deleted = 0";
 		$resultID = mysql_query($query, $linkID) or die("Data not found."); 
+		if(mysql_num_rows($resultID)==0){
+			print "The map either does not exist or has been deleted. Query was: $query";
+			return false;
+		}
+		
 		//Set up the basics of the XML.
 		header("Content-type: text/xml");
 		$xmlstr = "<?xml version='1.0' ?>\n<map></map>";
 		$xml = new SimpleXMLElement($xmlstr);
-		if(mysql_num_rows($resultID)==0){
-			$fail=$xml->addChild("error");
-			$fail->addAttribute("text", "The map either does not exist or has been deleted. Query was: $uquery");
-			return false;
-		}
-
 		$row = mysql_fetch_assoc($resultID);
 		$xml->addAttribute("ID", $row['map_id']);
 		$xml->addAttribute("username", $row['username']);
