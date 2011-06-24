3c54c5240ce6055b3e63abecf730e004a51007ed
Joshua Justice <Joshua.Justice@gatech.edu>
2011-06-24 11:00:00 -0400

    Move the XML instantiation around so the error will show

--- php/list_maps.php
+++ php/list_maps.php
@@ -7,14 +7,15 @@
 	mysql_select_db("agora", $linkID) or die ("Could not find database");
 	$query = "SELECT * FROM maps INNER JOIN users ON users.user_id = maps.user_id  AND maps.is_deleted=0";
 	$resultID = mysql_query($query, $linkID) or die("Data not found."); 
+	header("Content-type: text/xml");
+	$xmlstr = "<?xml version='1.0' ?>\n<list></list>";
+	$xml = new SimpleXMLElement($xmlstr);
 	if(mysql_num_rows($resultID)==0){
 		$fail=$output->addChild("error");
 		$fail->addAttribute("text", "There are no maps in the list! Query was: $uquery");
 		return false;
 	}
-	header("Content-type: text/xml");
-	$xmlstr = "<?xml version='1.0' ?>\n<list></list>";
-	$xml = new SimpleXMLElement($xmlstr);
+	
 	for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){ 
 		$row = mysql_fetch_assoc($resultID);
 		if($row['is_deleted']){
