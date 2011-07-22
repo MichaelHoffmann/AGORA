a92fe448983b1a295b05558f49b25df72082c088
Joshua Justice <Joshua.Justice@gatech.edu>
2011-07-22 14:07:13 -0400

    Bugfix on the error message

--- php/my_maps.php
+++ php/my_maps.php
@@ -30,7 +30,7 @@
 	$userID = mysql_real_escape_string($_REQUEST['uid']);
 	$pass_hash = mysql_real_escape_string($_REQUEST['pass_hash']);
 	
-	header("Content-type: text/xml");
+	header("Content-type: text/xml");	
 	$xmlstr = "<?xml version='1.0' ?>\n<list></list>";
 	$xml = new SimpleXMLElement($xmlstr);
 	
@@ -38,14 +38,14 @@
 	mysql_select_db($dbName, $linkID) or die ("Could not find database");
 	if(!checkLogin($userID, $pass_hash, $linkID)){
 		$fail=$xml->addChild("error");
-		$fail->addAttribute("Login failed!");
+		$fail->addAttribute("text", "Login failed!");
 		print ($xml->asXML());
 		return false;
 	}
 	
 	$query = "SELECT * FROM maps INNER JOIN users ON users.user_id = maps.user_id WHERE maps.user_id=$userID AND maps.is_deleted=0 ORDER BY maps.title";
 	$resultID = mysql_query($query, $linkID) or die("Data not found."); 
-	if(mysql_num_rows($resultID)==0){
+	if(!$resultID or mysql_num_rows($resultID)==0){
 		$fail=$xml->addChild("error");
 		$fail->addAttribute("There are no maps in the list! Query was: $query");
 		print ($xml->asXML());
