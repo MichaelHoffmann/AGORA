845840bfaf6382c1541847ae88164e88091e9022
Joshua Justice <Joshua.Justice@gatech.edu>
2011-07-25 12:03:22 -0400

    Update version datestamp

--- php/hashpass.php
+++ php/hashpass.php
@@ -1,6 +1,6 @@
 <?php
 	
-	$password = $_REQUEST['password'];  //TODO: Change this back to a GET when all testing is done.
+	$password = $_REQUEST['password'];
 	$salted_pass = $password . "AGORA";
 	$hashed_pass = mysql_real_escape_string(md5($salted_pass));
 	print $hashed_pass;
