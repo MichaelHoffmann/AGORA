0d572602850df112ddc7fb541af8ec0298a8bcdb
Joshua Justice <Joshua.Justice@gatech.edu>
2011-07-25 12:22:07 -0400

    CheckLogin no longer die()s. Error handling is the caller\'s responsibility.

--- php/checklogin.php
+++ php/checklogin.php
@@ -21,14 +21,15 @@
 	*/
 	/**
 	* Check-Login function separated out for widespread use.
+	* Note that error handling is the caller's responsibility.
 	*/
 	function checkLogin($userID, $pass_hash, $linkID)
 	{
 		$userclause = mysql_real_escape_string("$userID");
 		$passclause = mysql_real_escape_string("$pass_hash");
 		$query = "SELECT * FROM users WHERE user_ID='$userID' AND password='$passclause'";
-		$resultID = mysql_query($query, $linkID) or die("Data not found."); 
-		if(mysql_num_rows($resultID)>0){
+		$resultID = mysql_query($query, $linkID);
+		if($resultID && mysql_num_rows($resultID)>0){
 			return true;
 		}else{
 			return false;
