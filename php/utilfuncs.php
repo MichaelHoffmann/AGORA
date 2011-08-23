0d3a9753e176bab5c55ca13a6f0f6815d70d5bff
Joshua Justice <Joshua.Justice@gatech.edu>
2011-08-23 14:54:23 -0400

    Refactoring begun...

--- php/utilfuncs.php
+++ php/utilfuncs.php
@@ -35,4 +35,27 @@
 			return false;
 		}
 	}
+	
+	/**
+	* Function for converting code to UTF-8.
+	* Licensed under the W3C Software License
+	* License can be found here: http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231
+	* Code comes from here: http://w3.org/International/questions/qa-forms-utf-8.html
+	*/
+	function to_utf8( $string ) {
+		if ( preg_match('%^(?:
+			[\x09\x0A\x0D\x20-\x7E]            # ASCII
+			| [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+			| \xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
+			| [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+			| \xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
+			| \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+			| [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+			| \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+			)*$%xs', $string) ) {
+			return $string;
+		} else {
+			return iconv( 'CP1252', 'UTF-8', $string);
+		}
+	}
 ?>
