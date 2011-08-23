7e76df89744a0c051612d94b340224967188db17
Joshua Justice <Joshua.Justice@gatech.edu>
2011-08-23 14:49:48 -0400

    Super fix applied, since the simple fix doesn't work on Linux. Now to clean up the mess this left.

--- php/insert.php
+++ php/insert.php
@@ -531,7 +531,24 @@ List of variables for insertion:
 		return $output;
 		
 	}
-	$xmlparam = iconv('', 'UTF-8', $_REQUEST['xml']); //TODO: Change this back to a GET when all testing is done.
+	function to_utf8( $string ) {
+		// From http://w3.org/International/questions/qa-forms-utf-8.html
+		if ( preg_match('%^(?:
+			[\x09\x0A\x0D\x20-\x7E]            # ASCII
+			| [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+			| \xE0[\xA0-\xBF][\x80-\xBF]         # excluding overlongs
+			| [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+			| \xED[\x80-\x9F][\x80-\xBF]         # excluding surrogates
+			| \xF0[\x90-\xBF][\x80-\xBF]{2}      # planes 1-3
+			| [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+			| \xF4[\x80-\x8F][\x80-\xBF]{2}      # plane 16
+			)*$%xs', $string) ) {
+			return $string;
+		} else {
+			return iconv( 'CP1252', 'UTF-8', $string);
+		}
+	}
+	$xmlparam = to_utf8($_REQUEST['xml']); //TODO: Change this back to a GET when all testing is done.
 	$userID = $_REQUEST['uid'];
 	$pass_hash = $_REQUEST['pass_hash'];
 	$output = insert($xmlparam, $userID, $pass_hash); 
